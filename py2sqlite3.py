e5cf59a48aa2671dd77bd6924340717e9d54ad21
Texmo2003 <59236096+Texmo2003@users.noreply.github.com>
2024-03-13 12:03:54 +0100

    bæsj

--- py2sqlite3.py
+++ py2sqlite3.py
@@ -1,4 +1,10 @@
 import sqlite3
 conn = sqlite3.connect('prosjekt.db')
 
-conn.execute("INSERT INTO")
+conn.execute("INSERT INTO Sal (salID, totaltAntallSeter) \
+            VALUES (1, 516 )");
+
+conn.execute("INSERT INTO Sal (salID, totaltAntallSeter) \
+            VALUES (2, 332 )");
+
+conn.commit()
