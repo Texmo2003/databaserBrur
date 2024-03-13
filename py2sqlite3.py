24cf58329006c8d432d9666365ae216d8e87be6b
Texmo2003 <59236096+Texmo2003@users.noreply.github.com>
2024-03-13 13:13:56 +0100

    make some tables

--- py2sqlite3.py
+++ py2sqlite3.py
@@ -1,10 +1,33 @@
 import sqlite3
-conn = sqlite3.connect('prosjekt.db')
+conn = sqlite3.connect('teater.db')
 
-conn.execute("INSERT INTO Sal (salID, totaltAntallSeter) \
-            VALUES (1, 516 )");
+conn.execute("DELETE FROM Sal");
 
+# Setter inn sal 1 (Hovedscenen) og sal 2 (Gamle scene) inn i Sal-tabellen
 conn.execute("INSERT INTO Sal (salID, totaltAntallSeter) \
-            VALUES (2, 332 )");
+            VALUES (1, 516)");
+conn.execute("INSERT INTO Sal (salID, totaltAntallSeter) \
+            VALUES (2, 332)");
+
+# Setter inn seter i sal 1 (Hovedscenen) i Sete-tabellen
+for stolnr in range(0, 524):
+    område = "parkett"
+    if (stolnr > 467 and stolnr < 472) or (stolnr > 495 and stolnr < 500):
+        continue
+    if stolnr < 505:
+        område = "galleri"
+    conn.execute("INSERT INTO Sete (stolNr, radNr, område, salID) \
+                    VALUES (?, ?, ?, 2)", (stolnr+1, (stolnr//28) + 1, område))
+
+# Setter inn seter i sal 2 (Lille Scene) i Sete-tabellen
+
+gamlescene = [[18,16,17,18,18,17,18,17,17,14], [28,27,22,17], [33,18,17]]
+omraader = ["parkett", "balkong", "galleri"]
+for omraade in range(len(gamlescene)):
+    for rad in range(len(gamlescene[omraade])):
+        for stolnr in range(0, int(gamlescene[omraade][rad])):
+            conn.execute("INSERT INTO Sete (stolNr, radNr, område, salID) \
+                #VALUES (?, ?, ?, 2)", (stolnr+1, rad+1, omraader[omraade]))
+            print(rad, omraader[omraade], stolnr+1)
 
 conn.commit()
