d0c5075ce86abe3658261cb2d8831c4b4e27d655
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-03-14 08:33:38 +0100

    fix: Suggestions⁉️

--- py2sqlite3.py
+++ py2sqlite3.py
@@ -1,13 +1,13 @@
 import sqlite3
 conn = sqlite3.connect('teater.db')
 
-conn.execute("DELETE FROM Sal");
+conn.execute("DELETE FROM Sal")
 
 # Setter inn sal 1 (Hovedscenen) og sal 2 (Gamle scene) inn i Sal-tabellen
 conn.execute("INSERT INTO Sal (salID, totaltAntallSeter) \
-            VALUES (1, 516)");
+            VALUES (1, 516)")
 conn.execute("INSERT INTO Sal (salID, totaltAntallSeter) \
-            VALUES (2, 332)");
+            VALUES (2, 332)")
 
 # Setter inn seter i sal 1 (Hovedscenen) i Sete-tabellen
 for stolnr in range(0, 524):
@@ -21,6 +21,7 @@ for stolnr in range(0, 524):
 
 # Setter inn seter i sal 2 (Lille Scene) i Sete-tabellen
 
+# ka den her gamle scenen variabelen e? e d liste over antall seter per rad per område liksom? føle d hadde vore nice me en kommentar ovenfor
 gamlescene = [[18,16,17,18,18,17,18,17,17,14], [28,27,22,17], [33,18,17]]
 omraader = ["parkett", "balkong", "galleri"]
 for omraade in range(len(gamlescene)):
@@ -31,3 +32,5 @@ for omraade in range(len(gamlescene)):
             print(rad, omraader[omraade], stolnr+1)
 
 conn.commit()
+
+# kanskje lage en sjekk som sjekker count(*) fra Sete og sammenligner med summen av seter i Sal? (for å sjekke at alle setene er lagt inn i Sete-tabellen)
