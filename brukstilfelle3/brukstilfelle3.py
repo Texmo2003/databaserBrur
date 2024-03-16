fc96a918a8a74fbc55380c66677dea5a482947fc
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-03-16 15:24:42 +0100

    fix: Tidied up brukstilfelle 3 code

--- brukstilfelle3/brukstilfelle3.py
+++ brukstilfelle3/brukstilfelle3.py
@@ -1,48 +1,71 @@
 import sqlite3
 conn = sqlite3.connect('teater.db')
 c = conn.cursor()
-with open("brukstilfelle3/brukstilfelle3.sql") as file:
-    sql_script = file.read()
-c.executescript(sql_script)
 
-c.execute('''
-    SELECT Sete.radNr, Sete.område, Sete.salID
-    FROM Sete LEFT JOIN (
-        SELECT stolNr, radNr, område, salID
-        FROM Billett INNER JOIN Ordre ON Billett.ordreID = Ordre.ordreID
-        WHERE Ordre.stykkeID = "Størst av alt er kjærligheten" AND Ordre.forestillingDato = '2024-02-03'
-    ) AS ReserverteSeter ON ReserverteSeter.stolNr = Sete.stolNr AND ReserverteSeter.radNr = Sete.radNr AND ReserverteSeter.område = Sete.område AND ReserverteSeter.salID = Sete.salID
-    WHERE ReserverteSeter.stolNr IS NULL AND Sete.salID = (SELECT salID FROM Stykke WHERE stykkeID = "Størst av alt er kjærligheten")
-    GROUP BY Sete.radNr, Sete.område
-    HAVING COUNT(*) >= 9
-    ORDER BY Sete.område, Sete.radNr
-''')
-available_row = c.fetchone()
+def get_available_rows_having_amt_seats (what_show, when, min_available_seats):
+    c.execute('''
+        SELECT Sete.radNr, Sete.område, Sete.salID
+        FROM Sete LEFT JOIN (
+            SELECT stolNr, radNr, område, salID
+            FROM Billett INNER JOIN Ordre ON Billett.ordreID = Ordre.ordreID
+            WHERE Ordre.stykkeID = ? AND Ordre.forestillingDato = ?
+        ) AS ReserverteSeter ON ReserverteSeter.stolNr = Sete.stolNr AND ReserverteSeter.radNr = Sete.radNr AND ReserverteSeter.område = Sete.område AND ReserverteSeter.salID = Sete.salID
+        WHERE ReserverteSeter.stolNr IS NULL AND Sete.salID = (SELECT salID FROM Stykke WHERE stykkeID = ?)
+        GROUP BY Sete.radNr, Sete.område
+        HAVING COUNT(*) >= ?
+        ORDER BY Sete.område, Sete.radNr
+    ''', (what_show, when, what_show, min_available_seats))
+    return c.fetchall()
 
-c.execute('''
-    SELECT Sete.stolNr, Sete.radNr, Sete.område, Sete.salID
-    FROM Sete LEFT JOIN (
-        SELECT stolNr, radNr, område, salID
-        FROM Billett INNER JOIN Ordre ON Billett.ordreID = Ordre.ordreID
-        WHERE Ordre.stykkeID = "Størst av alt er kjærligheten" AND Ordre.forestillingDato = '2024-02-03'
-    ) AS ReserverteSeter ON ReserverteSeter.stolNr = Sete.stolNr AND ReserverteSeter.radNr = Sete.radNr AND ReserverteSeter.område = Sete.område AND ReserverteSeter.salID = Sete.salID
-    WHERE ReserverteSeter.stolNr IS NULL AND Sete.radNr = ? AND Sete.område = ? AND Sete.salID = ?
-''', (available_row[0], available_row[1], available_row[2]))
+def get_available_seats_in_row (row_number, area, salID):
+    c.execute('''
+        SELECT Sete.stolNr, Sete.radNr, Sete.område, Sete.salID
+        FROM Sete LEFT JOIN (
+            SELECT stolNr, radNr, område, salID
+            FROM Billett INNER JOIN Ordre ON Billett.ordreID = Ordre.ordreID
+            WHERE Ordre.stykkeID = "Størst av alt er kjærligheten" AND Ordre.forestillingDato = '2024-02-03'
+        ) AS ReserverteSeter ON ReserverteSeter.stolNr = Sete.stolNr AND ReserverteSeter.radNr = Sete.radNr AND ReserverteSeter.område = Sete.område AND ReserverteSeter.salID = Sete.salID
+        WHERE ReserverteSeter.stolNr IS NULL AND Sete.radNr = ? AND Sete.område = ? AND Sete.salID = ?
+    ''', (row_number, area, salID))
+    return c.fetchall()
 
-available_seats = c.fetchall()
+def get_total_price_for_all_tickets_of_same_type (show, orderID, groupName):
+    c.execute('''
+        SELECT SUM(PrisForForestilling.pris)
+        FROM (Billett INNER JOIN Ordre ON Billett.ordreID = Ordre.ordreID) INNER JOIN PrisForForestilling ON Ordre.stykkeID = PrisForForestilling.stykkeID
+        WHERE PrisForForestilling.gruppeNavn = ? AND Ordre.stykkeID = ? AND Ordre.ordreID = ? AND Billett.gruppeNavn = ?
+    ''', (groupName, show, orderID, groupName))
+    return c.fetchone()[0]
 
-for i in range(9):
-    c.execute("INSERT INTO Billett (billettNr, ordreID, gruppeNavn, stolNr, radNr, område, salID) VALUES (?, 3, 'Ordinær', ?, ?, ?, ?)", (i+1, available_seats[i][0], available_seats[i][1], available_seats[i][2], available_seats[i][3]))
-c.execute("UPDATE Ordre SET antallBilletter = (SELECT COUNT(*) FROM Billett WHERE ordreID = 3) WHERE ordreID = 3")
+def order_tickets_for_forestilling (orderID, groupName, available_seats, amtTicketsToOrder, forestillingDato, stykkeID, kundeID):
+    c.execute("DELETE FROM Billett WHERE ordreID = ?", (orderID, ))
+    c.execute("DELETE FROM Ordre WHERE ordreID = ?", (orderID, ))
+    c.execute("INSERT INTO Ordre (ordreID, kjøpsdato, kjøpstid, antallBilletter, kundeID, forestillingDato, stykkeID) VALUES (?, '2024-02-02', '12:00', 0, ?, ?, ?)", (orderID, kundeID, forestillingDato, stykkeID))
+    for i in range(amtTicketsToOrder):
+        c.execute("INSERT INTO Billett (billettNr, ordreID, gruppeNavn, stolNr, radNr, område, salID) VALUES (?, ?, ?, ?, ?, ?, ?)", (i+1, orderID, groupName, available_seats[i][0], available_seats[i][1], available_seats[i][2], available_seats[i][3]))
+    c.execute("UPDATE Ordre SET antallBilletter = (SELECT COUNT(*) FROM Billett WHERE ordreID = ?) WHERE ordreID = ?", (orderID, orderID))
 
-c.execute('''
-    SELECT SUM(PrisForForestilling.pris)
-    FROM (Billett INNER JOIN Ordre ON Billett.ordreID = Ordre.ordreID) INNER JOIN PrisForForestilling ON Ordre.stykkeID = PrisForForestilling.stykkeID
-    WHERE PrisForForestilling.gruppeNavn = 'Ordinær' AND Ordre.stykkeID = "Størst av alt er kjærligheten" AND Ordre.ordreID = 3
-''')
+def get_total_price_when_ordering_amt_tickets_in_same_row (stykkeID, forestillingDato, amtTicketsToOrder, groupName):
+    with open("brukstilfelle3/brukstilfelle3.sql") as file:
+        sql_script = file.read()
+    c.executescript(sql_script)
 
-total_price = c.fetchone()[0]
-print("Totalpris:", total_price, "NOK")
+    available_rows = get_available_rows_having_amt_seats("Størst av alt er kjærligheten", '2024-02-03', 9)
+
+    selected_row = available_rows[0]
+    selected_row_number = selected_row[0]
+    selected_area = selected_row[1]
+    selected_salID = selected_row[2]
+
+    available_seats = get_available_seats_in_row(selected_row_number, selected_area, selected_salID)
+
+    order_tickets_for_forestilling(3, "Ordinær", available_seats, 9, '2024-02-03', "Størst av alt er kjærligheten", 69)
+
+    total_price = get_total_price_for_all_tickets_of_same_type("Størst av alt er kjærligheten", 3, "Ordinær")
+
+    return total_price
+
+get_total_price_when_ordering_amt_tickets_in_same_row("Størst av alt er kjærligheten", '2024-02-03', 9, "Ordinær")
 
 conn.commit()
 conn.close()
--- brukstilfelle3/brukstilfelle3.sql
+++ brukstilfelle3/brukstilfelle3.sql
@@ -1,8 +1,4 @@
-DELETE FROM Billett WHERE ordreID = 3;
-DELETE FROM Ordre WHERE ordreID = 3;
 DELETE FROM Kunde WHERE kundeid = 69;
 
 -- Dummy kunde
 INSERT INTO Kunde (kundeID, tlfNr, navn, adresse) VALUES (69, 12345678, 'Ola Nordmann', 'Olasvei 1');
-
-INSERT INTO Ordre (ordreID, kjøpsdato, kjøpstid, antallBilletter, kundeID, forestillingDato, stykkeID) VALUES (3, '2024-02-02', '12:00', 0, 69, '2024-02-03', 'Størst av alt er kjærligheten');
