e1893bdd4529d8d38c73e39e7c4809e57a33cda2
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-03-19 13:36:04 +0100

    add: Brukstilfelle 6 done

---
+++ brukstilfelle6/brukstilfelle6.py
@@ -0,0 +1,11 @@
+import sqlite3
+conn = sqlite3.connect('teater.db')
+c = conn.cursor()
+with open("brukstilfelle6/brukstilfelle6.sql") as file:
+    sql_script = file.read()
+c.execute(sql_script)
+queryResult = c.fetchall()
+for i in queryResult:
+    print(i)
+conn.commit()
+conn.close()
